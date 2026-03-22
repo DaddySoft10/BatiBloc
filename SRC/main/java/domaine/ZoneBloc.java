50a557a752ed591987f452358305605f08d8e184
DaddySoft <aymanoelhamri@gmail.com>
2026-03-22 15:16:41 -0400

    Ajoute les types ZoneBloc et ZoneClassique

--- SRC/main/java/domaine/ZoneBloc.java
+++ SRC/main/java/domaine/ZoneBloc.java
@@ -1,4 +1,17 @@
 package domaine;
 
-public class ZoneBloc {
+public class ZoneBloc extends Zone {
+    private static final String TYPE_ZONE = "BLOC";
+
+    public ZoneBloc() {
+        super();
+    }
+
+    public ZoneBloc(double x, double y, double largeur, double hauteur, TypeForme typeForme) {
+        super(x, y, largeur, hauteur, typeForme);
+    }
+
+    public String getTypeZone() {
+        return TYPE_ZONE;
+    }
 }
